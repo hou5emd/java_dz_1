cc20846b1881c9dd960758c4d5ed273f93072fac
hou5emd <andrandrey92@gmail.com>
2025-08-19 20:25:17 +0500

    dz07

---
+++ src/homeworks/homework07/Product.java
@@ -0,0 +1,73 @@
+package homeworks.homework07;
+
+import java.util.Objects;
+
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Название не может быть пустым");
+        }
+        if (name.matches("\\d+")) {
+            throw new IllegalArgumentException("Название не должно содержать только цифры");
+        }
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("Название должно быть не короче 3 символов");
+        }
+        if (price <= 0) {
+            throw new IllegalArgumentException("Стоимость продукта должна быть больше нуля");
+        }
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Название не может быть пустым");
+        }
+        if (name.matches("\\d+")) {
+            throw new IllegalArgumentException("Название не должно содержать только цифры");
+        }
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("Название должно быть не короче 3 символов");
+        }
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException("Стоимость продукта должна быть больше нуля");
+        }
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + price + ")";
+    }
+
+    @Override
+    public boolean equals(Object input) {
+        if (this == input)
+            return true;
+        if (input == null || getClass() != input.getClass())
+            return false;
+        Product product = (Product) input;
+        return Double.compare(product.price, price) == 0 && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
